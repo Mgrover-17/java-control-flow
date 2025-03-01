9957452869a372d9172c7ad0c11a3c78d5ad5aa8
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-01 14:43:50 +0530

    [MANVI GROVER] ADD: Checked if number is positive negative or zero

---
+++ CheckNumber.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class CheckNumber{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt();
+		if(number>0){
+			System.out.println("The number "+number+" is positive.");
+		}
+		else if(number<0){
+			System.out.println("The number "+number+" is negative.");
+		}
+		else{
+			System.out.println("The number "+number+" is zero.");
+		}
+	}
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+			
