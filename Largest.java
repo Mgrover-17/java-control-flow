a9b09745c2b8e8349d0b5528720c3f96b19a85a0
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-01 14:12:38 +0530

    [MANVI GROVER] MOD: Check largest of three

--- Largest.java
+++ Largest.java
@@ -7,7 +7,9 @@ class Largest{
 		int num2=sc.nextInt();
 		int num3=sc.nextInt();
 		
-		System.out.println(largestOfThree(num1,num2,num3));
+		System.out.println("Is the first number the largest? "+largestOfThree(num1,num2,num3));
+		System.out.println("Is the second number the largest? "+largestOfThree(num2,num3,num1));
+		System.out.println("Is the third number the largest? "+largestOfThree(num3,num2,num1));
 	}
 	public static boolean largestOfThree(int n1,int n2,int n3){
 		if(n1>n2 && n1>n3){
