efba714eedb44d0e86b63042936c57f9450e1f1c
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-03 15:08:58 +0530

    [MANVI GROVER] ADD: Calculated the greatest factor beside the input taken using while loop.

---
+++ GreatestFactorWhile.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; // Import Scanner class for user input
+
+class GreatestFactorWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+		
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+
+        int greatestFactor = 1; // Initialize variable to store the greatest factor
+		int i=number-1; // Start checking from number - 1
+
+        // Loop from number-1 to 1 to find the greatest factor
+        while(i >= 1) {
+            // Check if 'i' is a factor of 'number'
+            if (number % i == 0) {
+                greatestFactor = i; // Store the first (largest) factor found
+                break; // Exit loop after finding the first factor
+			}
+			i--;
+		}
+
+        // Print the greatest factor found
+        System.out.println("Greatest factor: " + greatestFactor);
+    }
+}
